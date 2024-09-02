92119d629b712abd1aca1de7463a29d7b8d68096
nicoestepa9 <nicoo.estepaa@gmail.com>
2024-09-02 17:36:35 -0300

    Product y OrderProduct actualizadas

--- demo/src/main/java/com/uade/demo/entity/OrderProduct.java
+++ demo/src/main/java/com/uade/demo/entity/OrderProduct.java
@@ -1,12 +1,16 @@
 package com.uade.demo.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Data;
 
+@Data
+@Entity
 public class OrderProduct {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- demo/src/main/java/com/uade/demo/entity/Product.java
+++ demo/src/main/java/com/uade/demo/entity/Product.java
@@ -1,5 +1,6 @@
 package com.uade.demo.entity;
 
+import java.util.List;
 import java.util.Map;
 
 import jakarta.persistence.Column;
@@ -7,6 +8,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
 
 @Data
@@ -15,16 +19,22 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column
+
     private String description;
-    @Column
+
     private double price;
-    @Column
+
     private Map<String, Integer> stock; // talle : stock del talle
-    @Column
+
     private String club;
-    @Column
+
     private String league;
+
+    @ManyToMany
+    @JoinTable(name = "product_category",
+        joinColumns = @JoinColumn(name = "product_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private List<Category> categories;
 
     public Product(String description, double price, String club, String league){
         this.description = description;
