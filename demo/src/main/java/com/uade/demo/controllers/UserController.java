581758627acb4d00b96655b2a58e349398540c05
NicoHernandezUade <nihernandez@uade.edu.ar>
2024-09-08 01:30:45 -0300

    Agrego metodo para actualizar password de User

--- demo/src/main/java/com/uade/demo/controllers/UserController.java
+++ demo/src/main/java/com/uade/demo/controllers/UserController.java
@@ -1,13 +1,17 @@
 package com.uade.demo.controllers;
 
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +40,16 @@ public class UserController {
         if (result.isPresent())
             return ResponseEntity.ok(result.get());
 
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("admin/users/{userId}/password/update")
+    public ResponseEntity<User> updatePassword(@PathVariable Long userId,@RequestBody Map<String, String> request) {
+        String newPassword = request.get("newPassword");
+        Optional<User> user = userService.updatePassword(userId, newPassword);
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        }
         return ResponseEntity.noContent().build();
     }
 }
--- demo/src/main/java/com/uade/demo/service/UserServiceImpl.java
+++ demo/src/main/java/com/uade/demo/service/UserServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
 import com.uade.demo.entity.User;
 import com.uade.demo.repository.UserRepository;
 
@@ -16,6 +16,8 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private final PasswordEncoder passwordEncoder = null;
+
     public Page<User> getUsers(PageRequest pageRequest) {
         return userRepository.findAll(pageRequest);
     }
@@ -23,5 +25,18 @@ public class UserServiceImpl implements UserService {
     @Override
     public Optional<User> getUserById(Long categoryId) {
         return userRepository.findById(categoryId);
+    }
+
+    @Override
+    public Optional<User> updatePassword(Long userId, String newPassword) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            String encodedPassword = passwordEncoder.encode(newPassword);
+            user.setPassword(encodedPassword);
+            userRepository.save(user);
+            return Optional.of(user);
+        }
+        return Optional.empty();
     }    
 }
